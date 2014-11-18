8d9769032b5d94cdf175125b21cabfaaea8bb484
BickfordA <aidan.bickford@gmail.com>
2014-11-18 15:12:16 -0700

    Outlined Minnen Alogorithm

--- src/multiVariateMotif/Minnen/Minnen.java
+++ src/multiVariateMotif/Minnen/Minnen.java
@@ -6,7 +6,28 @@ import data.DoubleTimeSeries;
 public class Minnen extends MotifMiner {
 
 	public void mineDataForMotifs(DoubleTimeSeries[] dataset) {
-		// TODO Auto-generated method stub
+		
+		//Colect all subsequences of length w from the time series (like time stamps )
+		
+		//Compute an estimate of the distribution between all of the non-trivial matches for each dimension
+		
+		//// (Pain in the Ass) Search for values alpha dn c (projection distance) which lead to a sparce collision matrix
+		
+		//compute the sax word length and aplhabet size for each dimension
+		
+		//Build the collision matrix using reandom projection over the SAX words
+		
+		//Enumerate the motifs based on the collision matrix
+		
+			// Find the best Collision matrix
+		
+			//Estimate the nieghborhood radius
+		
+			//Locate all other occurences of this motif 
+		
+			// Remove subsequences that would constitute trivaial matches with the occurences of this motif
+		
+		
 		
 	}
 
