5e7deda76074e493c1813d0f5989e5b1337b0e35
BickfordA <aidan.bickford@gmail.com>
2014-11-28 13:36:46 -0700

    Statistical Significance

--- src/data/MultiDimensionalMotif.java
+++ src/data/MultiDimensionalMotif.java
@@ -6,9 +6,13 @@ public class MultiDimensionalMotif {
 	
 	protected ArrayList<SingleDimensionalMotif> _subMotifs;
 	protected int _count;
+	protected double _statisticalSignificance;
 	
 	
 	public void setCount(int count){ _count = count;}
 	public int getCount(){ return _count;}
 	public ArrayList<SingleDimensionalMotif>getMotifs() {return _subMotifs;}
+	
+	public void setStatSig(double value){ _statisticalSignificance = value;}
+	public double getStatSig(){ return _statisticalSignificance;}
 }
--- src/statisticalSignificance/StatSigUtil.java
+++ src/statisticalSignificance/StatSigUtil.java
@@ -3,28 +3,50 @@ package statisticalSignificance;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.commons.math3.util.MathUtils;
+
 import data.MultiDimensionalMotif;
 import data.Sequence;
+import data.SingleDimensionalMotif;
 import data.Symbol;
 
 public class StatSigUtil {
 	
 	static void assignStatisicalSignificance(ArrayList<MultiDimensionalMotif> motifsFound, Sequence[] sequenceData)
 	{
+		ArrayList< HashMap< Integer, Integer> >  dimensionalSymbolCount = new ArrayList< HashMap< Integer, Integer> >();
+		for(Sequence dimension: sequenceData){
+			HashMap< Integer , Integer> symbolCount = countChars(dimension);
+			dimensionalSymbolCount.add(symbolCount);
+		}
 		
-		
+		for(MultiDimensionalMotif m: motifsFound){
+			double significance = 1;
+			for(SingleDimensionalMotif sdm : m.getMotifs()){
+				//compute the probability
+				double subSignificance = 0;
+				int possibleWordCount = sequenceData[sdm.getStream()].size() - sdm.getTimeSeries().size();
+				int motifCount = motifCount(sdm.getTimeSeries(), sequenceData[sdm.getStream()]);
+				
+				double prob = calculateBernoulliProbability(dimensionalSymbolCount.get(sdm.getStream()), sdm, possibleWordCount);
+				
+				double permutations = permutaionCount(possibleWordCount, sdm.getTimeSeries().size());
+				for(int i = 0; i < motifCount; i ++){
+					subSignificance = permutations * Math.pow(prob, i) * Math.pow(1 - prob, possibleWordCount - sdm.getTimeSeries().size());
+				}
+				
+				significance *= subSignificance;
+			}
+			m.setStatSig(significance);
+		}
 	}
 	
-	//M1
-	public double calculateBernoulliProbability(Sequence motif, Sequence inputSequence){
-		double probability = 0; 
+	
+	//M1 get the counts for all of the different symbols
+	public static HashMap<Integer, Integer> countChars( Sequence inputSequence){
 		
 		HashMap<Integer, Integer> symbolCount = new HashMap<Integer, Integer>();
 		
-		//get a list of all of the unique symbols
-		for(Symbol s: motif.getSequence()){
-			symbolCount.put(s.getSymbol(), 0);
-		}
 		
 		int symbol;
 		//count the instances of each symbol
@@ -33,11 +55,58 @@ public class StatSigUtil {
 			if(symbolCount.containsKey(symbol)){
 				int count = symbolCount.get(symbol) + 1;
 				symbolCount.put(symbol,count);
+			}else{
+				symbolCount.put(symbol, 0);
 			}
 		}
 		
 		
-		return probability;
+		return symbolCount;
+	}
+	
+	public static double calculateBernoulliProbability(HashMap< Integer, Integer> symbolCounts, SingleDimensionalMotif motif, int totalCount )
+	{
+		double prob = 1; 
+		
+		//multiply the number of words together. 
+		for(Symbol s: motif.getTimeSeries().getSequence()){
+			int v = s.getSymbol();
+			prob *= symbolCounts.get(v);
+		}
+		
+		return prob /totalCount;
+	}
+	
+	public static int motifCount(Sequence motif, Sequence fullSeries)
+	{
+		int count = 0; 
+		
+		for(int i = 0; i < fullSeries.size() - motif.size(); i ++){
+			boolean match = true;;
+			for(int j = 0 ; j < motif.size(); j ++){
+				match &= (motif.getSequence().get(j).getSymbol() == fullSeries.getSequence().get(i + j).getSymbol());
+			}
+			if (match){
+				count ++;
+			}	
+		}
+		
+		return count;
 	}
 
+	public static int permutaionCount(int number, int chosen)
+	{
+		int returnVal = 1;
+		int numberSub = number;
+		for(int i = 0 ; i < chosen; i ++){
+			returnVal *= numberSub;
+			numberSub --;
+		}
+		int chosenFactorial = 1;
+		for(int i = 1; i <= chosen; i ++ ){
+			chosenFactorial *= i; 
+		}
+		
+		return returnVal / chosenFactorial;
+	}
 }
